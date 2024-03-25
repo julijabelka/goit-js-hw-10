d124df41aae0ef76374cec343f1ce2896542203b
julijabelka <julija.belka@gmail.com>
2024-03-25 12:03:45 +0100

    add js module flatpickr

---
+++ src/js/1-timer.js
@@ -0,0 +1,18 @@
+// Підключення бібліотек
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+// 
+// =====================================================
+
+// =====================================================
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+// Ініціалізація бібліотеки
+const flatpickr = new flatpickr('[type="text"]', options); 
